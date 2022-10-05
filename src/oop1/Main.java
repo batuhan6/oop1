0a03b189a8253c699ab5c21d49b9d69937322b66
Batuhan Baykal <batuhanbaykal666@gmail.com>
2022-10-05 09:41:15 +0200

    Private variables, get set. Inheritance. Polymorphism. Holding reference types.

--- src/oop1/Main.java
+++ src/oop1/Main.java
@@ -4,39 +4,56 @@ public class Main {
 
 	public static void main(String[] args) {
 		Product product1 = new Product();
-		product1.name="Delonghi Kahve Makinasi";
-		product1.unitPrice=7500;
-		product1.discount=7;
-		product1.unitsInStock=3;
-		product1.imageUrl="bilmemne.jpg";
+		product1.setName("Delonghi Kahve Makinasi");
+		product1.setUnitPrice(7500);
+		product1.setDiscount(7);
+		product1.setUnitsInStock(3);
+		product1.setImageUrl("bilmemne.jpg");
 		
 		
 		
 		Product product2 = new Product();
-		product2.name="Smeg Kahve Makinasi";
-		product2.unitPrice=6500;
-		product2.discount=8;
-		product2.unitsInStock=2;
-		product2.imageUrl="bilmemne2.jpg";
+		product2.setName("Smeg Kahve Makinasi");
+		product2.setUnitPrice(6500);
+		product2.setDiscount(8);
+		product2.setUnitsInStock(2);
+		product2.setImageUrl("bilmemne2.jpg");
 		
 		
 		
 		Product product3 = new Product();
-		product3.name="Kitchen Aid Kahve Makinasi";
-		product3.unitPrice=4500;
-		product3.discount=9;
-		product3.unitsInStock=4;
-		product3.imageUrl="bilmemne3.jpg";
+		product3.setName("Kitchen Aid Kahve Makinasi");
+		product3.setUnitPrice(4500);
+		product3.setDiscount(9);
+		product3.setUnitsInStock(4);
+		product3.setImageUrl("bilmemne3.jpg");
 		
 		
 		
 		Product[] products = {product1, product2, product3};
 		
+		System.out.println("<ul>");
 		for (Product product : products) {
-			System.out.println(product.name);
+			System.out.println("<li>" + product.getName() + "</li>");
 		}
-		
+		System.out.println("</ul>");
 
+		IndividualCustomer individualCustomer = new IndividualCustomer();
+		individualCustomer.setId(1);
+		individualCustomer.setPhone("05435243553");
+		individualCustomer.setCustomerNumber("12345");
+		
+		individualCustomer.setFirstName("Batuhan");
+		individualCustomer.setLastName("Baykal");
+		
+		CorporateCustomer corporateCustomer = new CorporateCustomer();
+		corporateCustomer.setId(2);
+		corporateCustomer.setCompanyName("BB");
+		corporateCustomer.setPhone("0532334542");
+		corporateCustomer.setTaxNumber("111111111111");
+		corporateCustomer.setCustomerNumber("54321");
+		
+		Customer[] customers = {individualCustomer, corporateCustomer};		
 	}
 
 }
